d49afb1e320c94ec832834b5cebf7c1680611b87
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2025-02-27 23:23:12 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://robertventures.com',
   integrations: [mdx(), sitemap()],
   devToolbar: {
     enabled: false
