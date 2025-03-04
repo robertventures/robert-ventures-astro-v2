5ef87da20b41e60e91d03438ab5eb721ec06929a
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2025-03-04 10:02:18 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+
 
 import netlify from '@astrojs/netlify';
 
