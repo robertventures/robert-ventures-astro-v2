136f6305039c6cbccd6ec06a59121dbc9d8b5552
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2025-03-31 12:30:19 -0400

    cleaning code

--- public/scripts/analytics/GA4ClarityTriggers.js
+++ public/scripts/analytics/GA4ClarityTriggers.js
@@ -1,6 +1,3 @@
-console.log("Hello from trigger");
-
-
 // Function to trigger events for Microsoft Clarity and GA4
 function triggerEvent(eventName, clarityEventName) {
     // Push event to Google Tag Manager's dataLayer
@@ -22,43 +19,5 @@ function triggerEvent(eventName, clarityEventName) {
     }
 }
 
-// Make triggerEvent globally accessible
-// window.triggerEvent = triggerEvent;
 
-// function initializeEventListeners() {
-//     console.log("Event triggers loaded from Direct GTM");
-
-//     // Track form submissions for .conversion_v1
-//     document.querySelectorAll('#form-cta').forEach(function (form) {
-//         form.addEventListener('submit', function () {
-//             triggerEvent('get_started_conversion', 'GetStarted');
-//         });
-//     });
-
-
-//     // // Track interactions with calculator inputs
-//     const calculatorInputs = document.querySelectorAll('.financial-future-section input');
-//     const hasInteracted = false;
-
-//     calculatorInputs.forEach(function (input) {
-//         input.addEventListener('click', function () {
-//             if (!hasInteracted) {
-//                 hasInteracted = true;
-//                 console.log('User interacted with the calculator');
-//                 triggerEvent('calculator_interaction', 'CalculatorInteraction');
-//             } else {
-//                 console.log('User already interacted with the calculator. Skipping.');
-//             }
-//         });
-//     });
-// }
-
-// Check if DOM is already loaded
-// if (document.readyState === 'loading') {
-//     console.log("Waiting for DOMContentLoaded...");
-//     document.addEventListener('DOMContentLoaded', initializeEventListeners);
-// } else {
-//     console.log("DOM already loaded, initializing listeners immediately");
-//     initializeEventListeners();
-// }
 
