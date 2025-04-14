b0ded5b646e97788d967cbdda0e44041fa9d19cd
Eduardo De Matos <62176573+nanudmr@users.noreply.github.com>
2025-04-13 20:02:50 -0400

    Chatbot code improvement

--- public/scripts/chatbot.js
+++ public/scripts/chatbot.js
@@ -1,12 +1,5 @@
-const excludedPaths = ['/signup', '/questions', '/booking'];
-const currentPath = "{currentPath}"; // Inject the server-side path into the client-side script
-
-console.log("Chatbot loaded")
-
-if (!excludedPaths.includes(currentPath)) {
-  var script = document.createElement('script');
-  script.src = "https://widgets.leadconnectorhq.com/loader.js";
-  script.setAttribute("data-resources-url", "https://widgets.leadconnectorhq.com/chat-widget/loader.js");
-  script.setAttribute("data-widget-id", "672a148805dded393695deca");
-  document.head.appendChild(script);
-}
+var script = document.createElement('script');
+script.src = "https://widgets.leadconnectorhq.com/loader.js";
+script.setAttribute("data-resources-url", "https://widgets.leadconnectorhq.com/chat-widget/loader.js");
+script.setAttribute("data-widget-id", "672a148805dded393695deca");
+document.head.appendChild(script);
